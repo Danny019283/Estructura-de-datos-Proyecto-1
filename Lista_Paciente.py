ba5d4394927d3291508bc641e8c24eabb130a84e
Danny AS <dannyalfaros45@gmail.com>
2025-09-06 19:18:44 -0600

    agregue la lista pacientes

---
+++ Lista_Paciente.py
@@ -0,0 +1,32 @@
+class Lista_Paciente:
+    def __init__(self):
+        self.__pacientes = []
+
+    def insertar_pacientes(self, paciente):
+        if paciente not in self.__pacientes:
+            self.__pacientes.append(paciente)
+            return True
+        return False
+
+    def actualizar_paciente(self, paciente):
+        for pac in self.__pacientes:
+            if pac == paciente:
+                pac = paciente
+                return True
+        return False
+
+    def borrar_paciente(self, cedula):
+        paciente = self.buscar_paciente(cedula)
+        if paciente is not None:
+            self.__pacientes.remove(paciente)
+            return True
+        return False
+
+    def buscar_paciente(self, cedula):
+        for paciente in self.__pacientes:
+            if paciente.cedula == cedula:
+                return paciente
+        return None
+
+    def ordenar_edad(self):
+        pass
